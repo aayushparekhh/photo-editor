e57bbeff738aee0eb66d4b01242afb08dc915886
Aayush <aayushparekhh@gmail.com>
2021-05-24 21:13:27 +0530

    Updates

---
+++ precache-manifest.8ebc1b35fa10fb71e12d74ad6ef470ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6468a2c7205110f3e548cb472b7cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b17b8f5d78d622a229",
+    "url": "/static/css/main.2d9177e9.chunk.css"
+  },
+  {
+    "revision": "dbb6f9bcb49dd7a583d8",
+    "url": "/static/js/2.0103e952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0103e952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b17b8f5d78d622a229",
+    "url": "/static/js/main.1ad5cb45.chunk.js"
+  },
+  {
+    "revision": "d9b54396f497012492f5",
+    "url": "/static/js/runtime-main.6d64c626.js"
+  }
+]);
